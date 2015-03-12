6489ea3e4f2e40e1819cc700840f60bb04d88530
charliepham <cpham97@microsoft.com>
2015-03-11 17:41:36 -0700

    Add Event web apis: Create/Update/Get/Delete

--- LetGoService/Controllers/EventController.cs
+++ LetGoService/Controllers/EventController.cs
@@ -4,36 +4,40 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using LetGoData;
 
 namespace LetGoService.Controllers
 {
     public class EventController : ApiController
     {
         // GET api/event
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //public IEnumerable<Event> Get()
+        //{
+        //    return null;
+        //}
 
         // GET api/event/5
-        public string Get(int id)
+        public Event Get(string id)
         {
-            return "value";
+            return DbManager.Instance.FindById<Event>(LetGoData.Event.CollectionName, id);
         }
 
         // POST api/event
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Event value)
         {
+            DbManager.Instance.Insert(value);
         }
 
         // PUT api/event/5
-        public void Put(int id, [FromBody]string value)
+        public void Put([FromBody]Event value)
         {
+            DbManager.Instance.Update(value);
         }
 
         // DELETE api/event/5
-        public void Delete(int id)
+        public void Delete(string id)
         {
+            DbManager.Instance.Delete(LetGoData.Event.CollectionName, id);
         }
     }
 }
